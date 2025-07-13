d99012c0ea066ebcd69aac862d6b754e5f66ff64
Michael Ferreira <michaeljf.2007@gmail.com>
2025-07-12 22:14:15 -0400

    fixed links

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,14 +3,14 @@ export const projects = [
         title: "Atari Centipede Remake",
         description:
             "A replica of the classic Atari Centipede game using Pygame.",
-        githubUrl: "https://github.com/michaeljf07/portfolio-website",
+        githubUrl: "https://github.com/michaeljf07/centipede",
         image: "/centipede.png",
     },
     {
         title: "Baobab",
         description:
             "A platform designed to make charitable giving simple, transparent, and impactful by directly connecting donors and charities.",
-        githubUrl: "https://github.com/michaeljf07/baobab",
+        githubUrl: "https://github.com/michaeljf07/baobab-website",
         image: "/baobab.png",
     },
     {
